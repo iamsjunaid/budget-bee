8c18bf56f4c5fa6b491a5e691b19e28a2c74b0df
Junaid Syed <sjunaid626@gmail.com>
2023-09-19 17:23:22 +0530

    Add associations : transaction.rb

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,8 @@
 class Transaction < ApplicationRecord
+  belongs_to :user
+  has_many :transaction_categories
+  has_many :categories, through: :transaction_categories
+
+  validates :name, presence: true
+  validates :amount, presence: true
 end
