b13f82e3d843efbfb8457d27c9a4f111ae36034f
Junaid Syed <sjunaid626@gmail.com>
2023-09-19 17:50:25 +0530

    Revert "Generate scaffold transactions"
    This reverts commit 3378ddf1a0de17eb98842a3e8b6071ce0b5fa506.

--- app/models/transaction.rb
+++
@@ -1,8 +0,0 @@
-class Transaction < ApplicationRecord
-  belongs_to :user
-  has_many :transaction_categories
-  has_many :categories, through: :transaction_categories
-
-  validates :name, presence: true
-  validates :amount, presence: true
-end
