b519b61476db78155524c608982759168ecbc5a7
Junaid Syed <sjunaid626@gmail.com>
2023-09-19 17:23:33 +0530

    Add associations : categoryrb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ApplicationRecord
+  belongs_to :user
+  has_many :transaction_categories
+  has_many :transactions, through: :transaction_categories
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
