f53fc98ce40108c93f9f33b75b3f070f5a69d3b4
Junaid Syed <sjunaid626@gmail.com>
2023-09-19 18:18:59 +0530

    Add splash homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :purchases
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "splash#index"
 end
