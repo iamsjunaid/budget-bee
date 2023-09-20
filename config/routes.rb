eab743401b1527cb7df3b983b5e38275772588a1
Junaid Syed <sjunaid626@gmail.com>
2023-09-20 13:05:09 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :categories
-  resources :purchases
-  resources :users
+  # resources :categories
+  # resources :purchases
+  # resources :users
+  # resources :purchase_categories
 
+  authenticated :user do
+    resources :categories do
+      resources :purchases
+    end
+  end
   # Defines the root path route ("/")
   root "splash#index"
 end
