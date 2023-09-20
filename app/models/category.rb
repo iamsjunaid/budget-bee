aa27b6e94e1914f80f902490fa324c41c52ee0d9
Junaid Syed <sjunaid626@gmail.com>
2023-09-20 17:39:36 +0530

    Update category

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,8 +3,6 @@ class Category < ApplicationRecord
   has_many :purchase_categories
   has_many :purchases, through: :purchase_categories
 
-  has_one_attached :icon
-
   validates :name, presence: true
   validates :icon, presence: true
 end
