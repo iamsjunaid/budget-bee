6c4f018046d6f0742d8a513e9435570db3b4eac4
Junaid Syed <sjunaid626@gmail.com>
2023-09-20 13:04:42 +0530

    Update purchases_controller.rb

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,70 +1,70 @@
 class PurchasesController < ApplicationController
-  before_action :set_purchase, only: %i[ show edit update destroy ]
+  before_action :set_purchase, only: %i[show edit update destroy]
 
-  # GET /purchases or /purchases.json
   def index
-    @purchases = Purchase.all
+    @category = Category.find(params[:category_id]) 
+    @purchases = current_user.purchases.order(created_at: :desc)
   end
 
-  # GET /purchases/1 or /purchases/1.json
   def show
+    @purchase = Purchase.find(params[:id])
   end
 
-  # GET /purchases/new
   def new
+    @category = Category.find(params[:category_id])
+    @categories = current_user.categories
     @purchase = Purchase.new
+    @selected_category_ids = []
   end
 
-  # GET /purchases/1/edit
-  def edit
-  end
-
-  # POST /purchases or /purchases.json
   def create
-    @purchase = Purchase.new(purchase_params)
+    primary_category_id = params[:purchase][:primary_category_id]
+    additional_category_ids = params[:additional_category_ids] || []
 
-    respond_to do |format|
-      if @purchase.save
-        format.html { redirect_to purchase_url(@purchase), notice: "Purchase was successfully created." }
-        format.json { render :show, status: :created, location: @purchase }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @purchase.errors, status: :unprocessable_entity }
+    name = params[:purchase][:name]
+    amount = params[:purchase][:amount]
+
+    @purchase = Purchase.new(name:, amount:, user: current_user)
+
+    if @purchase.save
+      PurchaseCategory.create(purchase_id: @purchase.id, category_id: primary_category_id)
+
+      additional_category_ids.each do |category_id|
+        next if category_id.blank?
+
+        PurchaseCategory.create(purchase_id: @purchase.id, category_id:)
       end
+
+      redirect_to category_path(primary_category_id), notice: 'Purchase was successfully created.'
+    else
+      @categories = current_user.categories
+      render :new
     end
   end
 
-  # PATCH/PUT /purchases/1 or /purchases/1.json
+  def edit; end
+
   def update
-    respond_to do |format|
-      if @purchase.update(purchase_params)
-        format.html { redirect_to purchase_url(@purchase), notice: "Purchase was successfully updated." }
-        format.json { render :show, status: :ok, location: @purchase }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @purchase.errors, status: :unprocessable_entity }
-      end
+    if @purchase.update(purchase_params)
+      @purchase.category_ids = params[:purchase][:category_ids]
+      redirect_to @purchase, notice: 'Purchase was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /purchases/1 or /purchases/1.json
   def destroy
     @purchase.destroy
-
-    respond_to do |format|
-      format.html { redirect_to purchases_url, notice: "Purchase was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to purchases_url, notice: 'Purchase was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_purchase
-      @purchase = Purchase.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def purchase_params
-      params.require(:purchase).permit(:name, :amount)
-    end
+  def set_purchase
+    @purchase = Purchase.find(params[:id])
+  end
+
+  def purchase_params
+    params.require(:purchase).permit(:name, :amount, :user_id, category_ids: [])
+  end
 end
