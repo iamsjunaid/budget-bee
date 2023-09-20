680bc27e4d9acdb02d2dfdd520275a3d4eb00960
Junaid Syed <sjunaid626@gmail.com>
2023-09-20 17:44:36 +0530

    Remove commented code

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # resources :categories
-  # resources :purchases
-  # resources :users
-  # resources :purchase_categories
 
   authenticated :user do
     resources :categories do
